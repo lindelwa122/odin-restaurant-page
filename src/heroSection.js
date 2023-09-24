be6b94824b25b0191590f1a800bd2e57cdff6ec6
Lindelwa <lindelwa.codes@gmail.com>
2023-09-24 19:44:23 +0200

    Add style on hero section

--- src/heroSection.js
+++ src/heroSection.js
@@ -1,3 +1,5 @@
+import restaurantImage from "./restaurant-image.jpeg";
+
 const hero = () => {
   const left = () => ({
     tagName: "div",
@@ -6,7 +8,10 @@ const hero = () => {
       innerHTML: `
         <h2>From Our Oven To Your Heart</h2>
         <p>Embracing Generations With Peanut Butter Love: Sharing our culinary legacy since 1828, where every cookie carries a taste of tradition and a dash of old-world charm.</p>
-      `
+      `,
+      style: {
+        maxWidth: "500px"
+      }
     }
   });
 
@@ -14,8 +19,11 @@ const hero = () => {
     const img = () => ({
       tagName: "img",
       options: {
-        src: "",
+        src: restaurantImage,
         alt: "Our first restaurant building",
+        style: {
+          height: "330px",
+        }
       },
     });
 
@@ -32,6 +40,15 @@ const hero = () => {
     tagName: "div",
     options: {
       classList: ["hero-section"],
+      style: {
+        alignItems: "center",
+        display: "flex",
+        height: "450px",
+        justifyContent: "space-between",
+        backgroundColor: "#1f2937",
+        color: "#e5e7eb",
+        padding: "20px 150px",
+      }
     },
     children: [left(), right()],
   };
